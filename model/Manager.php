5ae1ded8c2e96862db6a5ce7f0328c8c368656fe
Xavier DAVID <xav.david28@gmail.com>
2020-10-18 18:03:05 +0200

    projet complet : router - models - views - controllers

--- model/Manager.php
+++ model/Manager.php
@@ -6,9 +6,8 @@ class Manager {
     protected function dbConnect(){
       // Connexion à la base de données blog_ecrivain //
 
-           $db = new PDO('mysql:host=localhost;dbname=xavdavid28_blog_ecrivain;charset=utf8', 'xavdavid28_admin', 'saI[?s}4X]hg', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+           $db = new PDO('mysql:host=localhost;dbname=xavdavid28_blog_ecrivain;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
            // Retour du résultat
            return $db;
-
     }
 }
